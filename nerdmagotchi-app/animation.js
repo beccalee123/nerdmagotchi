9926d61f6e02a169607c22a5b334e35c39ebe9e8
niesssiobhan <niesssiobhan@live.com>
2019-02-18 18:27:12 -0800

    working on animation

---
+++ nerdmagotchi-app/animation.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Animated, View } from 'react-native';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.excitedAnimation = new Animated.Value(0)
+  }
+}
+
+excitedNerdling = () => {
+  Animation.stagger
+}
+
+Animated.delay(
+  this.state.
+)
